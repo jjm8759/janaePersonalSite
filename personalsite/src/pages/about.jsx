de14b3ffacd9dc77ec16cc910f0ace2b534680e3
jjm8759 <89434744+jjm8759@users.noreply.github.com>
2024-01-07 21:55:44 -0500

    Add Icons to Intro

--- personalsite/src/pages/about.jsx
+++ personalsite/src/pages/about.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 import '../styles/about.css';
-import Portrait from '../janaes_portrait.jpg';
 
+import Portrait from '../images/janaes_portrait.png';
+import GitIcon from '../images/github-mark.svg';
+import LinkedInIcon from '../images/linkedin-50.png'
 const About = () => {
     return (
         <div className='about'>
@@ -15,8 +17,8 @@ const About = () => {
                     <h3>Software Engineer</h3>
                 </div>
                 <div className='container-item'>
-                    <a href='https://www.linkedin.com/in/iyani-walcott-311b06227/'>LinkedIn</a>
-                    <a href='https://github.com/jjm8759'>Github</a>
+                    <a  href='https://www.linkedin.com/in/iyani-walcott-311b06227/'><img id='socials' src={LinkedInIcon} alt={"LinkedIn"}></img></a>
+                    <a  href='https://github.com/jjm8759'> <img id='socials' src={GitIcon} alt={"Github"}></img></a>
                 </div>
 
                 <div className='container-item' id='description'>
